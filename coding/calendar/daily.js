a7da02b900a39e61841c167221b92768092693d8
Arina Polyanskaya <apoly003@fiu.edu>
2023-12-04 16:32:05 +0100

    Unfinished changes

---
+++ coding/calendar/daily.js
@@ -0,0 +1,53 @@
+// Get today's date
+//const today = new Date();
+//const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+//const formattedDate = today.toLocaleDateString('en-US', options);
+
+// Display today's date in the header
+//document.getElementById('currentDate').innerText = formattedDate;
+
+// Sample events for the day
+//const events = [
+//  { time: '09:00 AM', title: 'Meeting' },
+//  { time: '01:00 PM', title: 'Lunch' },
+  // Add more events as needed
+//];
+
+// Function to display events
+//function displayEvents() {
+//  const eventsList = document.getElementById('eventsList');
+
+//  events.forEach(event => {
+//    const eventDiv = document.createElement('div');
+//    eventDiv.classList.add('event');
+
+//    const timeSpan = document.createElement('span');
+//    timeSpan.classList.add('time');
+//    timeSpan.innerText = event.time;
+
+//    const titleSpan = document.createElement('span');
+//    titleSpan.classList.add('event-title');
+//    titleSpan.innerText = event.title;
+
+//    eventDiv.appendChild(timeSpan);
+//    eventDiv.appendChild(titleSpan);
+
+//    eventsList.appendChild(eventDiv);
+//  });
+//}
+
+// Call the function to display events
+//displayEvents();
+
+
+//don't need this
+
+//function day() {
+//    const selected_day = document.querySelector('dot');
+//    if (selected_day.innerHTML === 'M') {
+//        document.querySelector('h1').innerHTML = 'Monday';
+//    }
+//    document.addEventListener('DOMContentLoaded', function(){
+//    document.querySelector('h1').innerHTML = #REFER TO THE INFORMATION FROM THE PREVIOUS PAGE
+//    });
+//    document.querySelector('dot').onclick = day;
