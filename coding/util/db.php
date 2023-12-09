1145ef6b10f44fbeab3a12657a0961bff8b19a5a
JozefKolek <100275065+JozefKolek@users.noreply.github.com>
2023-12-09 21:49:19 +0100

    db connect
    new online db on virtual server created. We have one together

--- coding/util/db.php
+++ coding/util/db.php
@@ -1,6 +1,7 @@
 <?php
-$dbconn = pg_connect("host=localhost dbname=candle user=postgres password=postgres"); 
+$dbconn = pg_connect("host=89.221.219.125 dbname=candle user=postgres password=HkqHsep2GQNT"); 
 if(!$dbconn) {
+    echo "scojcos";
     echo "<p> Coulud not connect to database". pg_last_error()."</p>";
 }
 
