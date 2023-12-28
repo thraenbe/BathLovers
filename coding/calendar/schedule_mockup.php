91a4304afb399b799e84a6f94077adf0e969d6d1
JozefKolek <100275065+JozefKolek@users.noreply.github.com>
2023-12-28 20:10:53 +0100

    weekly-view
    adding weekly possibility to watch event week by week

--- coding/calendar/schedule_mockup.php
+++ coding/calendar/schedule_mockup.php
@@ -11,14 +11,28 @@
 if (isset($_SESSION['user'])) {
     $username = $_SESSION['user'];
     $user_id = get_user_id($dbconn,$username);
-    $classes = get_registred_classes($dbconn,$user_id);
-    $non_school_events = get_nonschool_events($dbconn,$user_id,'2023-12-01','2024-01-24');
+    $classes = get_registred_classes($dbconn,$user_id);    
     $generated_weeks = generateWeeks('2023-05-29','2024-09-01');      
+    $num_weeks = sizeof($generated_weeks);
+    $actual_week = isset($_SESSION['actual_week']) ? $_SESSION['actual_week'] : (int) $num_weeks/2;    
+    $non_school_events = get_nonschool_events($dbconn,$user_id,$generated_weeks[$actual_week]['startDate'],$generated_weeks[$actual_week]['endDate']);
     // chronologically sorted on date interval you type    
     $all_events = sort_all_events($classes,$non_school_events);    
     // stayed old showing events
   
-    echo "<form method='post'>";   
+    echo "<form method='post'>";
+    ?>
+    <input type="hidden" name="actual_week" value="<?php echo $actual_week; ?>">    
+    <input type="submit" name="left_click" value="<">
+    <div>
+        <?php
+        $start = explode("-",$generated_weeks[$actual_week]['startDate']); 
+        $end = explode("-",$generated_weeks[$actual_week]['endDate']);
+        echo "Week $start[2].$start[1].$start[0] - $end[2].$end[1].$end[0]";
+        ?>
+    </div>    
+    <input type="submit" name="right_click" value=">">
+    <?php
     get_classes_table($dbconn, $classes); 
     get_events_table($dbconn, $non_school_events);
     if (sizeof($classes) > 0 || sizeof($non_school_events) > 0) {
@@ -26,23 +40,40 @@ if (isset($_SESSION['user'])) {
     } 
     echo "</form>";
 
-    if(isset($_POST["remove"] ) && isset($_POST['class'])) {
-        foreach ($_POST['class'] as $class) {
-            delete_course($dbconn, $class, $user_id);
-            echo "Succesfully deleted '$class'";
+    if(isset($_POST["remove"] )) {
+        if (isset($_POST['class'])){
+            foreach ($_POST['class'] as $class) {
+                delete_course($dbconn, $class, $user_id);
+                echo "Succesfully deleted '$class'";
+            }
+            echo "<meta http-equiv='refresh' content='0'>";
+        }        
+        if (isset($_POST['nonevent'])){
+            foreach ($_POST['nonevent'] as $event) {        
+                delete_nonschool_event($dbconn, $event);
+                echo "Succesfully deleted '$event'";
+            }    
+            echo "<meta http-equiv='refresh' content='0'>";
+            echo "Succesfully deleted selected items";
         }
+    } else if (isset($_POST['left_click'])){
+        $actual_week = $_POST['actual_week'];    
+        $actual_week--;    
+        if ($actual_week == -1) {
+            $actual_week = $num_weeks-1;
+        }    
+        $_SESSION['actual_week'] = $actual_week;
         echo "<meta http-equiv='refresh' content='0'>";
-    } 
-    if(isset($_POST["remove"] ) && isset($_POST['nonevent'])) {
-        foreach ($_POST['nonevent'] as $event) {        
-            delete_nonschool_event($dbconn, $event);
-            echo "Succesfully deleted '$event'";
-        }
-
+    } else if (isset($_POST['right_click'])){
+        $actual_week = $_POST['actual_week'];    
+        $actual_week++;    
+        if ($actual_week == $num_weeks) {
+            $actual_week = 0;
+        }    
+        $_SESSION['actual_week'] = $actual_week;
         echo "<meta http-equiv='refresh' content='0'>";
-        echo "Succesfully deleted selected items";
-
-    }     
+    }
+      
 }
 else {
     echo "<img src='../images/ComeniusUniversity.png' alt='University'>";
