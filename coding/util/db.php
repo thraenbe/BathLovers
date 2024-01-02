f3291a74b34b651dcb9eb9e383dfc8706dfade6c
JozefKolek <100275065+JozefKolek@users.noreply.github.com>
2024-01-02 20:01:18 +0100

    fix: sidenav
    change on schedule despite schedulue

--- coding/util/db.php
+++ coding/util/db.php
@@ -1,9 +1,7 @@
 <?php
-$dbconn = pg_connect("host=89.221.219.125 dbname=candle user=postgres password=HkqHsep2GQNT"); 
-if(!$dbconn) {
-    echo "scojcos";
+// $dbconn = pg_connect("host=89.221.219.125 dbname=candle user=postgres password=HkqHsep2GQNT"); 
+$dbconn = pg_connect("host=localhost dbname=candle user=postgres password=abc-123"); 
+if(!$dbconn) {    
     echo "<p> Coulud not connect to database". pg_last_error()."</p>";
 }
-
-
 ?>
