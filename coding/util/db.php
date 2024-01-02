148cb0cce75fc73ec338a343a98d071f2d0ef413
JozefKolek <100275065+JozefKolek@users.noreply.github.com>
2024-01-02 20:38:38 +0100

    db giving back
    sorry

--- coding/util/db.php
+++ coding/util/db.php
@@ -1,6 +1,6 @@
 <?php
-// $dbconn = pg_connect("host=89.221.219.125 dbname=candle user=postgres password=HkqHsep2GQNT"); 
-$dbconn = pg_connect("host=localhost dbname=candle user=postgres password=abc-123"); 
+$dbconn = pg_connect("host=89.221.219.125 dbname=candle user=postgres password=HkqHsep2GQNT"); 
+// $dbconn = pg_connect("host=localhost dbname=candle user=postgres password=abc-123"); 
 if(!$dbconn) {    
     echo "<p> Coulud not connect to database". pg_last_error()."</p>";
 }
